13e6f43f017bed851595fefdfc5b35264b0f36cd
Bill <bill.llach1801@gmail.com>
2022-12-05 22:15:50 -0500

    Do exeercise 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// EJERCICIO 1
 Console.WriteLine("Ingrese su nombre ");
 string nombre = Console.ReadLine();
 Console.WriteLine("Ingrese su apellido ");
@@ -10,3 +10,26 @@ string mensaje = "Saludos.";
 
 Console.WriteLine("Hola " + nombre + " " + apellido + ", tienes " + edad + " años. " + mensaje);
 
+// EJERCICIO 2
+//Coche
+int numDoors = 4;
+Console.WriteLine(numDoors);
+
+int numWheels = 4;
+Console.WriteLine(numWheels);
+
+string brand = "Audi";
+Console.WriteLine(brand);
+
+int itvVigente = 123454321;
+Console.WriteLine(itvVigente);
+
+//Mesa
+float weight = 23.4f;
+Console.WriteLine(weight);
+
+float large = 443.1f;
+
+string material = "Wood";
+
+string colour = "Brown";

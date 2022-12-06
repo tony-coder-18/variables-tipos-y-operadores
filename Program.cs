642de00f37a12bd55cec7da50ecdfb08c5d9ec3e
Bill <bill.llach1801@gmail.com>
2022-12-05 21:54:25 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese su nombre ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ingrese su apellido ");
+string apellido = Console.ReadLine();
+Console.WriteLine("Ingrese su edad ");
+string edad = Console.ReadLine();
+
+string mensaje = "Saludos.";
+
+Console.WriteLine("Hola " + nombre + " " + apellido + ", tienes " + edad + " años. " + mensaje);
+
